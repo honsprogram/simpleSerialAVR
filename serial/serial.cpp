ab6c546bad2d3ae2b1c17fc93a83bdf67f6c67c0
honprogram <37972188+honprogram@users.noreply.github.com>
2018-04-02 10:51:34 +0430

    review

--- serial/serial.cpp
+++ serial/serial.cpp
@@ -36,92 +36,6 @@ SOFTWARE.
 #include <string.h>
 #include "uartHZ.c" 
 #include "uartHZ.h" 
-  FILE * uart_str;
-bool volatile serial_flag=1;
-
-
-// This flag is set on USART Receiver buffer overflow
-unsigned char rx_buffer_overflow={0},rx_packet_ready={0};
-unsigned char rx_wr_index={0},rx_rd_index={0},rx_counter={0};
-unsigned char recev_state={0};
-unsigned char Packet_error={0};
-#define FRAMING_ERROR0 			(1<<FE0)
-#define DATA_OVERRUN0 			(1<<DOR0)
-#define DATA_REGISTER_EMPTY0 	(1<<UDRE0)
-#define TX_COMPLETE0 	 		(1<<TXC0)
-// USART Receiver buffer
-#define UART0_RX_BUFFER_SIZE 300
-
-	typedef struct {
-		unsigned char rx[UART0_RX_BUFFER_SIZE];
-	}__attribute((packed)) BUFFERTYPE0;
-	
-	BUFFERTYPE0 bufferUART0;
-// Interrupt serial ISR 
- ISR (USART0_RX_vect)
- {
-	 unsigned char status,data;
-	 status=UCSR0A;
-	 data=UDR0;
-	 ReceivedByte=data;
-	if ((status & (FRAMING_ERROR0 | DATA_OVERRUN0 ))==0)
-	{
-		if(data=='\r')
-		{
-			bufferUART0.rx[rx_wr_index++]='\0';
-			rx_wr_index=0;
-			serial_flag=0;
-		}
-		else
-		{
-			bufferUART0.rx[rx_wr_index]=data;
-			if (++rx_wr_index == UART0_RX_BUFFER_SIZE) rx_wr_index=0;
-			if (++rx_counter == UART0_RX_BUFFER_SIZE)
-			{
-				rx_counter=0;
-				rx_buffer_overflow=1;
-			}
-		}
-
-
-
-	}
-	
- 
- }
- // Serial lib
- class hzserial {
-	public:
-	 hzserial(void);
-    uint8_t * read(void);
-	bool available(void);
- };
- hzserial::hzserial() {
- }
- 
-bool hzserial::available(void){
-	return serial_flag;	
-}
-
-uint8_t * hzserial::read(void){
-		uint8_t * buff;
-		cli();
-		buff=bufferUART0.rx;
-		//bufferUART0.rx[0]='\0';
-		serial_flag=1;
-		sei();
-		return buff;
-		bufferUART0.rx[0]='\0';
-}
-
-hzserial Serial = hzserial();
-
- void uart_init(void) {
-	 UCSR0B =  (1 << RXEN0) | (1 << TXEN0) | (1<<RXCIE0);
-	 UCSR0C = (1<<UCSZ00)|(1 << UCSZ01);
-	 UBRR0H=0x00;
-	 UBRR0L=0x67;
- }
 
 int main(void)
 {
@@ -132,16 +46,13 @@ int main(void)
 	uart_init();
 	uart_str = fdevopen(uart_putchar, uart_getchar); //send , receive functions
 	stdout = uart_str;
-	printf("serial\n");
+	printf("serial simple avr\n");
     while(1)
     {
-	//	printf("serial\n");
-	//printf("sret= %d \n",Serial.available());
 	if(!Serial.available()) {
 		printf("%s ",Serial.read());
-		PORTC=0x80;
 	}
-	//PORTC=~PORTC;
+	PORTC=~PORTC;
 	_delay_ms(500);
     }
 }
--- serial/uartHZ.c
+++ serial/uartHZ.c
@@ -3,7 +3,6 @@
  *
  *  Author:  Hossein Zamanian (hznewresearch@gmail.com)
  */ 
-#include <stdio.h>
 #define F_CPU 16000000
 #define BAUD 9600
 #include <util/delay.h>
@@ -11,8 +10,57 @@
 #include <avr/io.h>
 #include <util/setbaud.h>
 #include <stdlib.h>
-extern volatile int uart_flag;
 #include "uartHZ.h"
+// buffer of UART0
+#define FRAMING_ERROR0 			(1<<FE0)
+#define DATA_OVERRUN0 			(1<<DOR0)
+#define DATA_REGISTER_EMPTY0 	(1<<UDRE0)
+#define TX_COMPLETE0 	 		(1<<TXC0)
+#define UART0_RX_BUFFER_SIZE 300
+extern volatile int uart_flag;
+bool volatile serial_flag=1;// This flag is set on USART Receiver buffer overflow
+
+unsigned char rx_buffer_overflow={0},rx_packet_ready={0};
+unsigned char rx_wr_index={0},rx_rd_index={0},rx_counter={0};
+unsigned char recev_state={0};
+unsigned char Packet_error={0};
+FILE * uart_str;
+
+typedef struct {
+	unsigned char rx[UART0_RX_BUFFER_SIZE];
+}__attribute((packed)) BUFFERTYPE0;
+BUFFERTYPE0 bufferUART0;
+
+class hzserial {
+	public:
+	hzserial(void);
+	uint8_t * read(void);
+	bool available(void);
+};
+
+hzserial::hzserial() {
+}
+
+bool hzserial::available(void){
+	return serial_flag;
+}
+
+unsigned char * hzserial::read(void){
+	cli();
+	serial_flag=1;
+	sei();
+	return bufferUART0.rx;
+}
+
+hzserial Serial = hzserial();
+
+void uart_init(void) {
+	UCSR0B =  (1 << RXEN0) | (1 << TXEN0) | (1<<RXCIE0);
+	UCSR0C = (1<<UCSZ00)|(1 << UCSZ01);
+	UBRR0H=0x00;
+	UBRR0L=0x67;
+}
+
 static int uart_putchar(char c, FILE *stream) ;
 char ReceivedByte=0;
  static int uart_putchar(char c, FILE *stream) {
@@ -38,6 +86,35 @@ char ReceivedByte=0;
 			 byte = (unsigned char) (b[i] & (1 << j));
 			 byte >>= j;
 			 printf("%u", byte);
+		 }
+	 }
+ } 
+ /*------interrupt handler-- interrupt service routine (ISR)------
+ -----------------------------------------------------------------
+ */
+ ISR (USART0_RX_vect)
+ {
+	 unsigned char status,data;
+	 status=UCSR0A;
+	 data=UDR0;
+	 ReceivedByte=data;
+	 if ((status & (FRAMING_ERROR0 | DATA_OVERRUN0 ))==0)
+	 {
+		 if(data=='\r')
+		 {
+			 bufferUART0.rx[rx_wr_index++]='\0';
+			 rx_wr_index=0;
+			 serial_flag=0;
+		 }
+		 else
+		 {
+			 bufferUART0.rx[rx_wr_index]=data;
+			 if (++rx_wr_index == UART0_RX_BUFFER_SIZE) rx_wr_index=0;
+			 if (++rx_counter == UART0_RX_BUFFER_SIZE)
+			 {
+				 rx_counter=0;
+				 rx_buffer_overflow=1;
+			 }
 		 }
 	 }
  }
